8a82747da6b2c77689642a0b62021c713c5d06b0
ahmed-147 <ahmed.fa.kh96@gmail.com>
2021-04-06 17:33:52 +0200

    starting remax front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { NavbarComponent } from './client/navbar/navbar.component';
+import { FooterComponent } from './client/footer/footer.component';
+import { HomeComponent } from './client/home/home.component';
+import { ProductsRowComponent } from './client/home/products-row/products-row.component';
+import { ProductCardComponent } from './client/home/products-row/product-card/product-card.component';
+import { CartComponent } from './client/cart/cart.component';
+import { CheckoutComponent } from './client/checkout/checkout.component';
+import { ShowProductsComponent } from './client/show-products/show-products.component';
+import { FiltersComponent } from './client/show-products/filters/filters.component';
+import { ContentComponent } from './client/show-products/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsRowComponent,
+    ProductCardComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShowProductsComponent,
+    FiltersComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/client/home/products-row/products-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-row',
+  templateUrl: './products-row.component.html',
+  styleUrls: ['./products-row.component.css']
+})
+export class ProductsRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
