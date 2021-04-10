c623cf5097bd9a7321270ff8b7d1ddca3c6af87b
Hassan rafat <Hassanrafat95@gmail.com>
2021-04-10 23:53:57 +0200

    cart and checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './client/cart/cart.component';
 import { Erorr404Component } from './client/erorr404/erorr404.component';
 import { HomeComponent } from './client/home/home.component';
+import { CheckoutComponent } from './client/checkout/checkout.component';
 
 const routes: Routes = [
   { path: 'home', component:  HomeComponent },
+  { path: 'cart', component:  CartComponent },
+  { path: 'checkout', component:  CheckoutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: Erorr404Component }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
