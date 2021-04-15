8eda884c7466c42fc36106798952a864cdb614db
abutalib44 <abutalib44@gmail.com>
2021-04-15 15:56:00 +0200

    show peoduct filters and content

--- src/app/client/show-products/filters/filters.component.ts
+++ src/app/client/show-products/filters/filters.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+enum CheckBoxType { ch_1, ch_2, ch_3,NONE };
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  check_box_type = CheckBoxType;
+
+  currentlyChecked: CheckBoxType;
+  selectCheckBox(targetType: CheckBoxType) {
+    // If the checkbox was already checked, clear the currentlyChecked variable
+    if(this.currentlyChecked === targetType) {
+      this.currentlyChecked = CheckBoxType.NONE;
+      return;
+    }
+
+    this.currentlyChecked = targetType;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
