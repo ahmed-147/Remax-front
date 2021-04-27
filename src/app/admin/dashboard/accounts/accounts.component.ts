307b7d33a7ee04e43ea5c1b41f2f78457e788a80
Mohammed Hamdy <mohammedayada1998@gmail.com>
2021-04-28 00:11:56 +0200

    update account services

--- src/app/admin/dashboard/accounts/accounts.component.ts
+++ src/app/admin/dashboard/accounts/accounts.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { IAccount } from './../../../model/interface/iaccount';
+import { AccountService } from './../../../service/account.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { element } from 'protractor';
+
+
 
 @Component({
   selector: 'app-accounts',
@@ -6,10 +12,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
+  addAccountForm: FormGroup;
+  updateAccountForm: FormGroup;
+  resetPasswordAccountForm: FormGroup;
+  img: any;
+  accounts: IAccount[];
+  imgDirectory : string = 'http://localhost:8000';
+  
+  @ViewChild('accountView') accountView: ElementRef;
+  @ViewChild('addaccountform') addaccountfrom: ElementRef;
+  @ViewChild('updateaccountform') updateaccountform: ElementRef;
+  @ViewChild('resetpasswordaccountform') resetpasswordaccountform: ElementRef;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,) {
+    this.addAccountForm = this.fb.group({
+      username: ['', [Validators.required]],
+      first_name: [''],
+      last_name: [''],
+      password: ['' , [Validators.required]],
+      password1: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      
+    });
 
-  ngOnInit(): void {
+  
+    //this.updateAccountForm = this.fb.group({
+     // first_name: [''],
+     // last_name: [''],
+     // phone: [''],
+    //});
+
+    //this.resetPasswordAccountForm = this.fb.group({
+     // password: [''],
+     // password1: [''],
+    //});
   }
 
+  ngOnInit(): void {
+    this.getAccounts();
+  }
+  getAccounts(){
+    this.accountService.getAllAccounts().subscribe(data=>{
+      console.log(data); 
+      this.accounts = data
+
+       },
+       err=>{
+         console.log(err);
+        
+       })
+  }
+  loadImg(event :any){
+    this.img = event.target.files[0]
+    
+  }
+  deleteFun(accountId){
+    
+    this.accountService.deleteAccountById(accountId).subscribe(data=>{
+    },
+    err=>{
+      console.log(err);
+    });
+    this.getAccounts();
+    
+  }
+  saveAccount(){
+
+  }
+  
 }
